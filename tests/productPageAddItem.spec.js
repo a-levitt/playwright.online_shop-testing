d1e2f6becdebad504326036af0e5ac588d1e87ea
a-levitt <artemis.levitt@gmail.com>
2024-07-27 14:12:42 +0200

    Add button locator as const, check cart count before adding an item

--- tests/productPageAddItem.spec.js
+++ tests/productPageAddItem.spec.js
@@ -2,9 +2,12 @@ import { test, expect } from "@playwright/test"
 
 test("Promo page add to basket", async ({ page }) => {
    await page.goto("https://datika.me/en/category/laptopovi/laptop-racunari/");
-   await page.getByRole('button').filter({ hasText: 'Add to cart' }).first().click();
 
    const cartCount = page.locator("//span[@class='cart_count count']");
+   await expect(cartCount).toHaveText("0");
+
+   const addToCartButton = await page.getByRole('button').filter({ hasText: 'Add to cart' }).first();
+   addToCartButton.click();
    await expect(cartCount).toHaveText("1");
 
    // await page.pause();
