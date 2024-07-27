781ee0d75a3d6a2cd60a9cc4e12ee3775c33fde2
a-levitt <artemis.levitt@gmail.com>
2024-07-27 14:06:24 +0200

    Expectation added

--- tests/productPageAddItem.spec.js
+++ tests/productPageAddItem.spec.js
@@ -1,7 +1,11 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 
 test("Promo page add to basket", async ({ page }) => {
    await page.goto("https://datika.me/en/category/laptopovi/laptop-racunari/");
    await page.getByRole('button').filter({ hasText: 'Add to cart' }).first().click();
-   await page.pause();
+
+   const cartCount = page.locator("//span[@class='cart_count count']");
+   await expect(cartCount).toHaveText("1");
+
+   // await page.pause();
 });
