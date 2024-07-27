4c76660152bccd1ad16c6141d61fa59471c8701c
a-levitt <artemis.levitt@gmail.com>
2024-07-27 15:02:25 +0200

    Add baseURL to config, add Cart locator

--- tests/productPageAddItem.spec.js
+++ tests/productPageAddItem.spec.js
@@ -1,14 +1,20 @@
 import { test, expect } from "@playwright/test"
 
 test("Promo page add to basket", async ({ page }) => {
-   await page.goto("https://datika.me/en/category/laptopovi/laptop-racunari/");
+   await page.goto("/en/category/laptopovi/laptop-racunari/");
+
 
    const cartCount = page.locator("//span[@class='cart_count count']");
+   await cartCount.waitFor();
    await expect(cartCount).toHaveText("0");
 
    const addToCartButton = await page.getByRole('button').filter({ hasText: 'Add to cart' }).first();
-   addToCartButton.click();
+   await addToCartButton.waitFor();
+   await addToCartButton.click();
    await expect(cartCount).toHaveText("1");
 
-   // await page.pause();
+   const orderCart = page.locator("//a[@id='cart']");
+   await orderCart.click();
+
+   //await page.pause();
 });
