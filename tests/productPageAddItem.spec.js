55900cc552f6ffef49d340222f01f3a6eed19ba5
a-levitt <artemis.levitt@gmail.com>
2024-07-27 13:25:11 +0200

    Fixed playwright locator

--- tests/productPageAddItem.spec.js
+++ tests/productPageAddItem.spec.js
@@ -2,6 +2,6 @@ import { test } from "@playwright/test"
 
 test("Promo page add to basket", async ({ page }) => {
    await page.goto("https://datika.me/en/category/laptopovi/laptop-racunari/");
-   await page.locator('form').filter({ hasText: '276 € Add to cart Lenovo' }).getByRole('button').click();
+   await page.getByRole('button').filter({ hasText: 'Add to cart' }).first().click();
    await page.pause();
 });
