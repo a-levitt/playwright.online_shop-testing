629307b11a477fdd664d22ed52f2bae89158c832
a-levitt <artemis.levitt@gmail.com>
2024-08-03 17:00:15 +0200

    Delete the cheapest product from cart method realization, Chekout page object

--- page-objects/Checkout.js
+++ page-objects/Checkout.js
@@ -3,7 +3,8 @@ export class Checkout {
         this.page = page;
         this.cartItems = page.locator("//div[@class='wa-products']/div");
         this.cartProductsPrices = page.locator("//div[@class='wa-price-total js-product-full-price']");
-        this.removeFromCartButtons = page.locator("//div[@data-title='Delete product from the cart']");
+        this.removeFromCartButtons = page.locator("//span[@data-title='Delete product from the cart']");
+        this.confirmRemoval = page.getByRole('button', { name: 'Delete', exact: true });
     }
 
     removeCheapestProduct =  async() => {
@@ -15,7 +16,19 @@ export class Checkout {
             const currencySignReplaced = element.replace(" €", "");
             return parseInt(currencySignReplaced, 10);
         })
-        console.warn({intPricesList});
+        //console.warn({intPricesList});
+        
+        const smallestPrice = Math.min(...intPricesList);
+        const smallestPriceIndex = intPricesList.indexOf(smallestPrice);
+        
+        const cheapesItemDeleteButton = this.removeFromCartButtons.nth(smallestPriceIndex);
+        await cheapesItemDeleteButton.waitFor();
+        await cheapesItemDeleteButton.click();
+        //await this.removeFromCartButtons.nth(smallestPriceIndex).waitFor();
+        //await this.removeFromCartButtons.nth(smallestPriceIndex).click();
+
+        await this.confirmRemoval.waitFor();
+        await this.confirmRemoval.click();
     }
 
 }
